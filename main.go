6edcb73acdf0b7dc0af706a07e1f87c7a248bc7e
Bieler96 <bieler15147@live.de>
2022-12-30 14:39:17 +0100

    Update main.go
    + create collections
    + reset collections
    +fill collections with data

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"log"
 
+	"github.com/Bieler96/Portfolio/utils"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"go.mongodb.org/mongo-driver/mongo/readpref"
@@ -24,4 +25,25 @@ func main() {
 	if err := client.Ping(ctx, readpref.Primary()); err != nil {
 		log.Fatal(err)
 	}
+}
+
+// create the collections for the database
+func createDB(client mongo.Client) {
+	utils.DB_NavEntries = client.Database("mydb").Collection("nav")
+	utils.DB_Projects = client.Database("mydb").Collection("projects")
+	utils.DB_Services = client.Database("mydb").Collection("services")
+}
+
+// reseted the database collections
+func resetDB(ctx context.Context) {
+	utils.ResetMongoNav(ctx)
+	utils.ResetMongoProject(ctx)
+	utils.ResetMongoService(ctx)
+}
+
+// fill the collections with initial data
+func initDB(ctx context.Context) {
+	utils.InitNavMongo(ctx)
+	utils.InitProjectMongo(ctx)
+	utils.InitServiceMongo(ctx)
 }

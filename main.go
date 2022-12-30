d5c3c1f192b09e4f5ef4df9575cc1032d38329d1
Bieler96 <bieler15147@live.de>
2022-12-30 14:36:07 +0100

    Connection

--- main.go
+++ main.go
@@ -1,5 +1,27 @@
 package main
 
-func main() {
+import (
+	"context"
+	"log"
 
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
+)
+
+func main() {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	// connection
+	opt := options.Client().ApplyURI("mongodb://root:rootpassword@localhost:27017")
+	client, err := mongo.Connect(ctx, opt)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err := client.Ping(ctx, readpref.Primary()); err != nil {
+		log.Fatal(err)
+	}
 }
